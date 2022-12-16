386b1da0c429a0b2847ffba617472d13d90f3f70
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-16 14:19:24 +0300

    99

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,12 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 void binary_tree_print(const binary_tree_t *tree);
 
 
