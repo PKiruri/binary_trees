11b8fa6424f13aec530977c003555208c406b787
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-16 13:07:02 +0300

    4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,37 +1,19 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
  *
- *  * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: the node to check
  *
- *   *
- *
- *    * @node: the node to check
- *
- *     *
- *
- *      * Return: 1 if node is a leaf, otherwise 0
- *
- *       */
-
-
+ * Return: 1 if node is a leaf, otherwise 0
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
-
 {
-
-		if (node)
-
-				{
-
-							if (!node->left && !node->right)
-
-											return (1);
-
-								}
-
-			return (0);
-
+	if (node)
+	{
+		if (!node->left && !node->right)
+			return (1);
+	}
+	return (0);
 }
