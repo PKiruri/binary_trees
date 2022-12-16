82f698dccf9389116e125bac8513d3db8fa964aa
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-16 13:06:26 +0300

    3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,33 +1,17 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
- *
- *  * binary_tree_delete - delete an entires binary tree
- *
- *   * @tree: tree to delete
- *
- *    * Return: void
- *
- *     */
-
+ * binary_tree_delete - delete an entires binary tree
+ * @tree: tree to delete
+ * Return: void
+ */
 void binary_tree_delete(binary_tree_t *tree)
-
 {
+	if (tree == NULL)
+		return;
 
-		if (tree == NULL)
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
 
-					return;
-
-
-
-			binary_tree_delete(tree->right);
-
-				binary_tree_delete(tree->left);
-
-
-
-					free(tree);
-
+	free(tree);
 }
